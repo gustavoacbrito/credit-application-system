363942ae2491311bb7f689b7590397d42dca143d
Gustavo Afonso Chaves de brito <gustavoacbrito@gmail.com>
2023-05-23 18:36:58 -0300

    commit

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/service/impl/CreditService.kt
@@ -0,0 +1,34 @@
+package me.dio.creditapplicationsystem.service.impl
+
+import me.dio.creditapplicationsystem.entity.Credit
+import me.dio.creditapplicationsystem.repository.CreditRepository
+import me.dio.creditapplicationsystem.service.ICreditService
+import org.springframework.stereotype.Service
+import java.lang.RuntimeException
+import java.util.*
+
+@Service
+class CreditService(
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
+    ):ICreditService {
+    override fun save(credit: Credit): Credit {
+        credit.apply { customer = customerService.findById(credit.customer?.id!!) }
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> {
+        TODO("...")
+    }
+
+    override fun findByCreditCode(customerId: Long,creditCode: UUID): Credit? {
+        val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
+            ?: throw RuntimeException("Credit code $creditCode not found"))
+        if (credit.customer?.id == customerId){
+            return credit
+        } else {
+            throw RuntimeException("Contact admin")
+        }
+    }
+
+}
