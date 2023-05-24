c1de8742dbfe830043e46989cdbeed7b5cefd7c8
Gustavo Afonso Chaves de brito <gustavoacbrito@gmail.com>
2023-05-24 14:48:47 -0300

    commit

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/response/CreditView.kt
@@ -0,0 +1,25 @@
+package me.dio.creditapplicationsystem.dto.response
+
+
+import me.dio.creditapplicationsystem.entity.Credit
+import me.dio.creditapplicationsystem.system.Status
+import java.math.BigDecimal
+import java.util.*
+
+data class CreditView(
+    val creditCode: UUID,
+    val creditValue: BigDecimal,
+    val numberOfInstallment: Int,
+    val status: Status,
+    val emailCustomer: String?,
+    val incomeCustomer: BigDecimal?
+) {
+    constructor(credit: Credit) : this(
+        creditCode = credit.creditCode,
+        creditValue = credit.creditValue,
+        numberOfInstallment = credit.numberOfInstallments,
+        status = credit.status,
+        emailCustomer = credit.customer?.email,
+        incomeCustomer = credit.customer?.income
+    )
+}
--- src/main/kotlin/me/dio/creditapplicationsystem/service/ICreditService.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/service/ICreditService.kt
@@ -1,10 +1,10 @@
 package me.dio.creditapplicationsystem.service
 
 import me.dio.creditapplicationsystem.entity.Credit
-import java.util.UUID
+import java.util.*
 
 interface ICreditService {
-    fun save(credit: Credit):Credit
+    fun save(credit: Credit): Credit
     fun findAllByCustomer(customerId: Long): List<Credit>
     fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
 }
